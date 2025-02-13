8e055d49bb50ffd127ffaf709b0328fcdaf36412
Sagar Gujarathi <sagargujarathi108@gmail.com>
2025-02-13 12:12:36 +0530

    implement API server initialization and environment variable loading

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"aspire-auth/internal/server"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DB_URL := os.Getenv("DB_CONNECTION_URL")
+	PORT_NUMBER := os.Getenv("PORT_ADDRESS")
+
+	if DB_URL == "" || PORT_NUMBER == "" {
+		log.Fatal("DB_CONNECTION_URL environment variable is not set")
+	}
+
+	server := server.NewAPIServer(DB_URL, PORT_NUMBER)
+
+	server.InitHandlers()
+
+	server.Run()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,37 @@
 package server
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 type APIServer struct {
 	databaseURL string
 	portAddress string
+	app         *fiber.App
+	database    *gorm.DB
+}
+
+func NewAPIServer(databaseURL string, portAddress string) *APIServer {
+	app := fiber.New()
+	database, err := gorm.Open(postgres.Open(databaseURL))
+
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	return &APIServer{app: app, database: database, databaseURL: databaseURL, portAddress: portAddress}
+}
+
+func (s *APIServer) InitHandlers() {
+	fmt.Println("Initializing handlers...")
+}
+
+func (s *APIServer) Run() {
+
+	log.Fatal(s.app.Listen(s.portAddress))
 }
